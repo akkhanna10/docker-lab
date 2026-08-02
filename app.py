7112f55f1cc70fb7a67f2272a8613411c77556f5
akkhanna10 <akkhanna0120@gmail.com>
2026-08-02 20:43:54 +0530

    updated app.py to log pod name for traffic

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import http.server
 import socketserver
+import socket
 
 PORT = 80
-Handler = http.server.SimpleHTTPRequestHandler
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        hostname = socket.gethostname()
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        message = f"Hello from pod: {hostname}\n"
+        self.wfile.write(message.encode())
+        print(f"Handled request, served by {hostname}")
+
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving on port {PORT}")
     httpd.serve_forever()
